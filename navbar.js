bbf6005a8b9e76365c61d33f61487da10285debe
Marlee Roth <marlee.roth@gmail.com>
2019-02-23 17:34:38 -0500

    remove modal js

--- navbar.js
+++ navbar.js
@@ -29,16 +29,7 @@ var lastId,
       if (item.length) { return item; }
     });
 
-// // Bind click handler to menu items
-// // so we can get a fancy scroll animation
-// menuItems.click(function(e){
-//   var href = $(this).attr("href"),
-//       offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-//   $('html, body').stop().animate({
-//       scrollTop: offsetTop
-//   }, 300);
-//   e.preventDefault();
-// });
+
 
 // Bind to scroll
 $(window).scroll(function(){
@@ -65,38 +56,6 @@ $(window).scroll(function(){
 
 
 
-
-$('.js-scroll-trigger').click(function() {
-  $('.navbar-collapse').collapse('hide');
-});
-
-
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 
 
