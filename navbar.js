1e1e3ef7df3d1438564c2f203b51c10c4c5d93c5
marleemaria <marlee.roth@gmail.com>
2019-09-05 13:44:19 -0400

    Close Nav hamburger on click

--- navbar.js
+++ navbar.js
@@ -54,22 +54,8 @@ $(window).scroll(function(){
    }
 });
 
-
-
-
-
-
+$( '#top-menu' ).on("click", function(){
+  $('.navbar-collapse').removeClass("show");
 });
 
-
- // function w3_open() {
- //   document.getElementById("main").style.marginLeft = "25%";
- //   document.getElementById("mySidebar").style.width = "25%";
- //   document.getElementById("mySidebar").style.display = "block";
- //   document.getElementById("openNav").style.display = 'none';
- // }
- // function w3_close() {
- //   document.getElementById("main").style.marginLeft = "0%";
- //   document.getElementById("mySidebar").style.display = "none";
- //   document.getElementById("openNav").style.display = "inline-block";
- // }
+});
